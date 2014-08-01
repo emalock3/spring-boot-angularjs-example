4ac61e95d1144e5c3121eac70f5db071a9ff13ac
emalock3 <emalock3@gmail.com>
2014-08-01 18:13:04 +0900

    Add ajax samples

--- src/main/resources/resources/js/controllers.js
+++ src/main/resources/resources/js/controllers.js
@@ -5,7 +5,19 @@ angular.module('myApp.controllers', [])
   .controller('IndexCtrl', ['$scope', function($scope) {
       $scope.now = new Date();
   }])
-  .controller('Page1Ctrl', ['$scope', function($scope) {
-      $scope.now1 = new Date();
+  .controller('Page1Ctrl', ['$scope', '$http', function($scope, $http) {
+      $http.get('api/date/now').success(function(data) {
+        $scope.nowFromServer = new Date(parseInt(data));
+      });
+  }])
+  .controller('Page2Ctrl', ['$scope', '$http', function($scope, $http) {
+      $http.get('api/test_entities/3').success(function(data) {
+        $scope.entity3 = data;
+      });
+  }])
+  .controller('Page3Ctrl', ['$scope', '$http', function($scope, $http) {
+      $http.get('api/test_entities').success(function(data) {
+        $scope.entities = data;
+      });
   }])
 ;
