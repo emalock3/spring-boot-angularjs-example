898d17c36d3b37f5d22386f889c165f33b1d045f
emalock3 <emalock3@gmail.com>
2014-08-08 12:59:12 +0900

    change baseURL

--- src/main/resources/resources/js/app.js
+++ src/main/resources/resources/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['ngRoute', 'ngAnimate', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
